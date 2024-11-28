b96b7e7028d67bad425bccadcd10c6aca94c33b4
Godbell <12191579@inha.edu>
2024-11-28 17:16:30 +0900

    Learn: thread

--- network/src/main/java/thread/DigitThread.java
+++ network/src/main/java/thread/DigitThread.java
@@ -1,10 +1,8 @@
 package thread;
 
-public class ThreadEx01 {
-
-	public static void main(String[] args) {
-		new AlphabetThread().start();
-
+public class DigitThread extends Thread {
+	@Override
+	public void run() {
 		for (int i = 1; i <= 9; i++) {
 			System.out.print(i);
 
@@ -14,6 +12,5 @@ public class ThreadEx01 {
 				e.printStackTrace();
 			}
 		}
-
 	}
 }
